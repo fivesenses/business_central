7455c0eb3bf800553b22b5fdac3a6d2d887ffc1c
Jordan Brock <jordan@brock.id.au>
2019-01-18 15:05:14 +0800

    Renaming module

--- lib/business_central/api_methods.rb
+++ lib/business_central/api_methods.rb
@@ -1,80 +1,80 @@
 ##
 # Provides an interface between the object methods and the API
 #
-module BusinessCentral::ApiMethods
-  def method_missing(method_name, *args, &block)
-    if supported_method?(method_name.to_sym)
-      self.send(method_name.to_sym, args)
-    else
-      "#{method_name} is not supported"
+module BusinessCentral
+  module ApiMethods
+    def method_missing(method_name, *args, &block)
+      if supported_method?(method_name.to_sym)
+        self.send(method_name.to_sym, args)
+      else
+        "#{method_name} is not supported"
+      end
     end
-  end
 
-  def get(*args)
-    if args.length == 0
-      find_collection
-    else
-      find_by_id(args[0]).first
+    def get(*args)
+      if args.length == 0
+        find_collection
+      else
+        find_by_id(args[0]).first
+      end
     end
-  end
 
-  def create(*args)
-    return if args.length == 0
-    post(args[0]).first
-  end
+    def create(*args)
+      return if args.length == 0
+      post(args[0]).first
+    end
 
-  def update(*args)
-    return if args.length == 0
-    patch(args[0], args[1], args[2]).first
-  end
+    def update(*args)
+      return if args.length == 0
+      patch(args[0], args[1], args[2]).first
+    end
 
-  def delete(*args)
-    return if args.length == 0
-    destroy(args[0], args[1])
-  end
+    def delete(*args)
+      return if args.length == 0
+      destroy(args[0], args[1])
+    end
 
-  private
-  def find_collection
-    response = @client.get("/#{api_object}")
-    handle_error(response)
-    process(response)
-  end
+    private
+    def find_collection
+      response = @client.get("/#{api_object}")
+      handle_error(response)
+      process(response)
+    end
 
-  def find_by_id(id)
-    response = @client.get("/#{api_object}/#{id}")
-    handle_error(response)
-    process(response)
-  end
+    def find_by_id(id)
+      response = @client.get("/#{api_object}/#{id}")
+      handle_error(response)
+      process(response)
+    end
 
-  def post(data)
-    response = @client.post("/#{api_object}", data)
-    handle_error(response)
-    process(response)
-  end
+    def post(data)
+      response = @client.post("/#{api_object}", data)
+      handle_error(response)
+      process(response)
+    end
 
-  def patch(id, etag, data)
-    response = @client.patch("/#{api_object}/#{id}", etag, data)
-    handle_error(response)
-    process(response)
-  end
+    def patch(id, etag, data)
+      response = @client.patch("/#{api_object}/#{id}", etag, data)
+      handle_error(response)
+      process(response)
+    end
 
-  def destroy(id, etag)
-    response = @client.delete("/#{api_object}/#{id}", etag)
-    handle_error(response)
-    response.code
-  end
+    def destroy(id, etag)
+      response = @client.delete("/#{api_object}/#{id}", etag)
+      handle_error(response)
+      response.code
+    end
 
-  def api_object
-    self.class.const_get(:API_OBJECT)
-  end
+    def api_object
+      self.class.const_get(:API_OBJECT)
+    end
 
-  def api_object_parent
-    self.class.const_get(:API_OBJECT_PARENT)
-  end
+    def api_object_parent
+      self.class.const_get(:API_OBJECT_PARENT)
+    end
 
-  def supported_method?(method_name)
-    self.class.const_get(:SUPPORTED_METHODS).include?(method_name)
+    def supported_method?(method_name)
+      self.class.const_get(:SUPPORTED_METHODS).include?(method_name)
+    end
   end
 end
-
-
