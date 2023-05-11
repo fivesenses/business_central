259264f721906b6f5e1736b498006113fae8de99
patrickreillydev <79639852+patrickreillydev@users.noreply.github.com>
2023-05-11 13:24:11 +1000

    Fixed Indentation
    Fixed Indentation  on fivenet_customer

--- lib/business_central/fivenet_customer.rb
+++ lib/business_central/fivenet_customer.rb
@@ -3,9 +3,9 @@
 # Used for the Pipelabs API Extension 
 #
 module BusinessCentral
-    class FivenetCustomer < BusinessCentral::Base
-      API_OBJECT = "FivenetCustomer"
-      SUPPORTED_METHODS = [:get, :create, :update, :delete, :collection_filter]
-    end
+  class FivenetCustomer < BusinessCentral::Base
+    API_OBJECT = "FivenetCustomer"
+    SUPPORTED_METHODS = [:get, :create, :update, :delete, :collection_filter]
   end
+end
   
